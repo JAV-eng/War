2b9eb461cad905cd2914459ec24b516fa48d048a
ViniciusLira1311 <52613474+ViniciusLira1311@users.noreply.github.com>
2024-09-07 16:37:55 -0300

    Criação da classe jogador e implementação da escolha de cor

---
+++ jogador.py
@@ -0,0 +1,14 @@
+class Jogador:
+    def __init__(cor_jogador, objetivo, carta_territorio):
+        self.carta_territorio = carta_territorio
+        self.objetivo = objetivo
+        self.cor_jogador = cor_jogador
+
+    def escolhe_cor(self, escolha):
+        cores_disponiveis = [[['Amarelo'],[]], [['Branco'],[]], [['Azul'],[]], [['Preto'],[]], [['Verde'],[]], [['Amarelo'],[]]]
+
+        if 1 <= escolha <= len(cores_disponiveis):
+            self.cor_jogador = cores_disponiveis[escolha - 1][0][0]
+            return f"Você escolheu a cor: {self.cor_jogador}"
+        else:
+            return "Escolha inválida. Tente novamente."
